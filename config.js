69285ce1981d2afffefddc550a2fec241ff825e1
Nimsara3838383 <112757253+Nimsara3838383@users.noreply.github.com>
2024-12-18 19:24:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,14 +5,14 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //paste your session_id
+SESSION_ID: process.env.SESSION_ID || "qjxy3DLZ#4KObv0ZoOooEDqkXiOVCThUoBeJR1JbtLo5QzXvKkt0", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 AUTO_REACT: process.env.AUTO_REACT || "true",
-FAKE_RECORDING: process.env.FAKE_RECORDING || "true",    
-ANTI_LINK: process.env.ANTI_LINK || "true",
+FAKE_RECORDING: process.env.FAKE_RECORDING || "false",    
+ANTI_LINK: process.env.ANTI_LINK || "false",
 ANTI_BAD: process.env.ANTI_BAD || "true",    
 };
